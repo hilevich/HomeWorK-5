060df66c6eb62ea11146051e6169b7da6f6d8357
yura <yura2007.31@gmail.com>
2016-03-03 00:21:15 +0200

    Create class main.java

---
+++ class main.java
@@ -0,0 +1,9 @@
+public class KeywordEnter {
+	 public static void main(String args[]) { 
+	      double temperature = 45;// цельсій
+	      double humidity = 90; // відсотків
+	      double lighting = 500; // моль на м2
+	 
+	 
+	        }
+	 }
