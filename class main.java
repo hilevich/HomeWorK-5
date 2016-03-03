9cf177be001c48e3f637841d7ac9897377be0a57
yura <yura2007.31@gmail.com>
2016-03-03 12:50:13 +0200

    Update class main.java

--- class main.java
+++ class main.java
@@ -3,6 +3,8 @@ package greenhouse;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 
 
 public class main {
@@ -22,59 +24,76 @@ public class main {
 		 onOffСontrol newControl = new onOffСontrol();
 		 control change = new control();
 		 
-		    onOffСontrol.massege query = newControl.new massege(); 
+		    onOffСontrol.massege onOffControlMassage = newControl.new massege(); 
 		 
 		 if (((maxTemperature > temperature)&&(minTemperature<temperature))&&((maxHumidity>humidity)&&(minHumidity<humidity))&&((maxLights>lights)&&(minLights<lights))){
-			sens.outSensor();
+			System.out.println("vale of greenhouse is:");
+			 sens.valeTemperature();
+			 sens.valehumidite();
+			 sens.valeLight();
 			
 			 
 		 }
 		 else
 		 {
-			 newControl.error();
-			 query.message1();
+			
+			 temperature = newControl.changeValeTemperature(temperature);
+			 humidity =newControl.changeValeHumidity(humidity);
+			 lights = newControl.changeValeLights(lights);
+			 onOffControlMassage.message1();
 		 }
 		
 		
-		
-		System.out.println("виберете что хотите зделать");
+		System.out.println("1 - change temperature; 2 - change humidity; 3 - change lights; 4 - exit ");
+		System.out.println("take your chose:");
 		int exit = 1;
 		while(exit!=0){
+			try{
 		Scanner scan= new Scanner(System.in);
         int vik=scan.nextInt();
+			if (vik>4||vik <0)
+			{
+				System.out.println("your chose don't true");
+			}
+    
+        switch (vik){
 		
-       switch (vik){
-		
-		case 1:{change.chengeTemperature(temperature);
-				
-		
-		
-		break;
-		}
-				
-		case 2:{System.out.println("виберете пункт 2");
-		change.chengeHumidity(humidity);
-		break;
-		}
+        			case 1:{System.out.println("1 - your chose change temperatur:");
+        				change.chengeTemperature(temperature);
 			
-		case 3:{System.out.println("виберете пункт 3");
-		change.chengeLights(lights);
-		
-		break;
-		}
+        				break;
+        			}
+				
+        				case 2:{System.out.println("2 - your chose change humidity");
+        				change.chengeHumidity(humidity);
+        				break;
+        			}
 			
-		case 4:{System.out.println("вихід");
+        				case 3:{System.out.println("3 - your chose change lights");
+        				change.chengeLights(lights);
 		
-		exit = 0;
-		break;
-		}
+        				break;
+        			}
+			
+        			    case 4:{System.out.println("4 - your chose exit");
+		
+        			    exit = 0;
+        			    break;
+        			 }
 
-       }
+        		}
+			}catch(Exception e)
+			{
+				JOptionPane.showMessageDialog(null, "your enter not true vale");
+			}
 		
 		}
-	
+		
+			
+		}
+		
 	
 	
 	}
 
-}
+
