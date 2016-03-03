807aed99858143cee2e232dbcdc44aba502a509d
yura <yura2007.31@gmail.com>
2016-03-03 14:31:24 +0200

    Update sensors.java

--- sensors.java
+++ sensors.java
@@ -1,10 +1,26 @@
 package greenhouse;
 
-public class sensors {
+public class control {
 
-	public void outSensor() {
+	public double changeTemperature(double temperature) {
+		
+		return temperature;
 		// TODO Auto-generated method stub
-		System.out.println("вивід показників сенсорів");
+		
 	}
+
+	public double changeHumidity(double humidity) {
+		return humidity;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public double changeLights(double lights) {
+		return lights;
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 
 }
