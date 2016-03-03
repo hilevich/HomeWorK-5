b18de10bc1bf5786b28f2aa939dc81a33bb631dd
TimurKALv <timurkirim@ukr.net>
2016-03-03 21:04:33 +0200

    Update sensors.java

--- sensors.java
+++ sensors.java
@@ -2,21 +2,21 @@ package greenhouse;
 
 public class sensors {
 
-	public void changeTemperature(double temperature) {
+	public void valeTemperature(double temperature) {
 		System.out.println("Temperature is changed to" + temperature + "degree" );
 		
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void changeHumidity(double humidity) {
+	public void valeHumidity(double humidity) {
 		System.out.println("Humidity is changed to" + humidity + "%");
 		
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void changeLights(double lights) {
+	public void valeLights(double lights) {
 		System.out.println("Lights is changed to" + lights + "1 mol/m2");
 		
 		// TODO Auto-generated method stub
