3386e3ddf0af31fba2960b7d105e093b9c143991
yura <yura2007.31@gmail.com>
2016-03-03 12:03:24 +0200

    Update sensors.java

--- sensors.java
+++ sensors.java
@@ -1,5 +1,10 @@
-class sensors{
+package greenhouse;
 
+public class sensors {
 
+	public void outSensor() {
+		// TODO Auto-generated method stub
+		System.out.println("вивід показників сенсорів");
+	}
 
 }
