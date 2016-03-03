b68b9ced2cace546c4bd60dd33597e1f7d19ccda
yura <yura2007.31@gmail.com>
2016-03-03 12:02:57 +0200

    Update control.java

--- control.java
+++ control.java
@@ -1,7 +1,24 @@
-class Сontrol{
+package greenhouse;
 
+public class control {
 
+	public double chengeTemperature(double temperature) {
+		
+		return temperature;
+		// TODO Auto-generated method stub
+		
+	}
 
+	public double chengeHumidity(double humidity) {
+		return humidity;
+		// TODO Auto-generated method stub
+		
+	}
 
+	public double chengeLights(double lights) {
+		return lights;
+		// TODO Auto-generated method stub
+		
+	}
 
 }
