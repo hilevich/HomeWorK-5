909a7e9b0690d109c863785186a9a2ddc113e6f5
yura <yura2007.31@gmail.com>
2016-03-03 12:03:51 +0200

    Update onOffСontrol.java

--- onOffСontrol.java
+++ onOffСontrol.java
@@ -1,7 +1,19 @@
-class onOffСontrol{
+package greenhouse;
 
+public class onOffСontrol {
 
+	public void error() {
+	System.out.println("помила");		
+	
+	
+	}
+	
+	
+	public class massege{
+		
+		void message1(){
+			System.out.println("смс");
+		}
+	}
 
- class massаge{
- }
- }
+}
