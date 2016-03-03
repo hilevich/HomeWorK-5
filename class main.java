d5f2335e71110288a1648844622bce31f1ec6d24
yura <yura2007.31@gmail.com>
2016-03-03 12:10:35 +0200

    Update class main.java

--- class main.java
+++ class main.java
@@ -1,13 +1,80 @@
-public class KeywordEnter {
-	 public static void main(String args[]) { 
-	     // temperature - - - - - - цельсій
-	     // humidity- - - - - -  відсотків
-	     // lighting   -   - - - моль на м2
-	      
-	        double temperature, humidity, lights;
+package greenhouse;
+
+
+import java.util.Scanner;
+
+
+
+public class main {
+	public static void main(String[] args) {
+		
+		double temperature, humidity, lights;
+		
 		double maxTemperature = 60, maxHumidity= 100, maxLights=500;
-		double minTemperature = 18, minHumidity= 100, minLights=500;
-	 
-	 
-	        }
-	 }
+		
+		double minTemperature = 18, minHumidity= 60, minLights=300;
+		
+		 temperature = 40;
+		 humidity = 90;
+		 lights = 600;
+		 
+		 sensors sens = new sensors();
+		 onOffСontrol newControl = new onOffСontrol();
+		 control change = new control();
+		 
+		    onOffСontrol.massege query = newControl.new massege(); 
+		 
+		 if (((maxTemperature > temperature)&&(minTemperature<temperature))&&((maxHumidity>humidity)&&(minHumidity<humidity))&&((maxLights>lights)&&(minLights<lights))){
+			sens.outSensor();
+			
+			 
+		 }
+		 else
+		 {
+			 newControl.error();
+			 query.message1();
+		 }
+		
+		
+		
+		System.out.println("виберете что хотите зделать");
+		int exit = 1;
+		while(exit!=0){
+		Scanner scan= new Scanner(System.in);
+        int vik=scan.nextInt();
+		
+       switch (vik){
+		
+		case 1:{change.chengeTemperature(temperature);
+				
+		
+		
+		break;
+		}
+				
+		case 2:{System.out.println("виберете пункт 2");
+		change.chengeHumidity(humidity);
+		break;
+		}
+			
+		case 3:{System.out.println("виберете пункт 3");
+		change.chengeLights(lights);
+		
+		break;
+		}
+			
+		case 4:{System.out.println("вихід");
+		
+		exit = 0;
+		break;
+		}
+
+       }
+		
+		}
+	
+	
+	
+	}
+
+}
