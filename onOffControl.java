ffe3f973444a23ff5bc9ef4eabd347da2ba1857f
yura <yura2007.31@gmail.com>
2016-03-04 09:25:40 +0200

    Update onOffControl.java

--- onOffControl.java
+++ onOffControl.java
@@ -1,44 +1,17 @@
-﻿package greenhouse;
+package greenhouse;
 
-public class onOffControl {	
+public class onOffСontrol {
+
+	
+	
+	public class massege{
 		
-	/**
-	 * 
-	 * @param temIndicator – the temperature indicator, if true – enabled, if false – disabled
-	 * @param humIndicator – the humidity indicator, if true – enabled, if false – disabled
-	 * @param ligIndicator – the lights indicator, if true – enabled, if false – disabled
-	 */
-	public void checkIndicator(boolean temIndicator, boolean humIndicator, boolean ligIndicator) {	
-		boolean[] indicatorStatus = new boolean[3];
-		
-		indicatorStatus[0] = temIndicator;
-		indicatorStatus[1] = humIndicator;
-		indicatorStatus[2] = ligIndicator;	
-		
-		new onOffControl.message().messageSMS(indicatorStatus);
-		
-		System.out.println("SMS sent!");
-	}	
-	/**
-	 * This class send SMS according to the indicator status 
-	 */
-	public class message {		
-		void messageSMS(boolean[] indicatorStatus) {
-			String[] sensor = new String[3];
-			sensor[0] = "The temperature sensor is ";
-			sensor[1] = "The humidity sensor is ";
-			sensor[2] = "The lights sensor is ";
-			for (int i = 0; i < indicatorStatus.length; i++) {
-				if (indicatorStatus[i]) {
-					System.out.println(sensor[i] + "ON");
-				} else {
-					System.out.println(sensor[i] + "OFF");
-				}
-			}			
+		void message1(double temperature, double humidity, double lights){
+			System.out.println("смс");
 		}
 	}
 
-	
+
 	public double changeValeTemperature(double temperature, double minTemperature, double maxTemperature) {
 		if (temperature>maxTemperature||temperature<minTemperature){
 			System.out.println("Temperature is too high \n We start normalizing temperature...");
@@ -87,11 +60,7 @@ public class onOffControl {
 			System.out.println("System failed to change lights");
 			return lights;
 		}
-		
-         		
-		
-	}
 
 
-
+}
 }
