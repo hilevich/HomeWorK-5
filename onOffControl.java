7b802c2b4de62fede7a49e1db9462419b5adbb8d
yura <yura2007.31@gmail.com>
2016-03-04 14:57:24 +0200

    Update onOffControl.java

--- onOffControl.java
+++ onOffControl.java
@@ -1,75 +1,172 @@
+/**
+ * Створити консольну систему управління домашньою оранжереєю;
+ * 1. В oранжереї можна регулювати температуру, вологість, освітлення;
+ * 2. Включення і виключення опалення, поливу і освітлення повинно бути автоматизовано;
+ * 3. у випадку якщо відповідні датчики показують перевищення або пониження
+заданих параметрів – повинно відправлятись відповідне повідомлення на
+мобільний телефон власника оранжереї;
+ * 4. Необхідна система підтримання життєзабезпечення саджанців має увімкнутися для відновлення
+комфортного рівня;
+ * 
+ * @version 1.0
+ * @author Yura
+*/
+
+
 package greenhouse;
+
 
 import java.util.Scanner;
 
-public class onOffСontrol {
 
+
+
+
+
+public class main {
+	public static void main(String[] args) {
+	
+		// changes initialization
+		double temperature, humidity, lights;
+		
+		double maxTemperature = 60, maxHumidity= 100, maxLights=500;
+		
+		double minTemperature = 18, minHumidity= 60, minLights=300;
+		
+		// random vale
+		
+		 temperature = 50;
+		 humidity = 90;
+		 lights = 1000;
+		 
+		 //constructor classes
+		 
+ 		 
+		 onOffСontrol newControl = new onOffСontrol();
+		 control change = new control();
+		 control.sensorsPrint sensorsPrintVale = change.new sensorsPrint();
+		
+		
+		 
+		 // massege vale of greenhouse else change vale and sent SMS 
+		    onOffСontrol.massege onOffControlMassage = newControl.new massege(); 
+		 
+		    // if all vale be true sent vale to console
+		 if ((maxTemperature > temperature)&&(minTemperature<temperature)&&(maxHumidity>humidity)&&(minHumidity<humidity)&&(maxLights>lights)&&(minLights<lights))
+		 {
+			 
+			System.out.println("vale of greenhouse is:");
+			sensorsPrintVale.valeTemperature(temperature);
+			sensorsPrintVale.valeHumidity(humidity);
+			sensorsPrintVale.valeLights(lights);
+			
+			 
+		 }
+		 else
+		 { 
+		 
+			if (!(maxTemperature > temperature)&&(minTemperature<temperature)){
+			double	outTemperature = temperature;
+			onOffControlMassage.messageTemperature(outTemperature, temperature);
+			System.out.println("");
+				temperature = newControl.changeValeTemperature(temperature, minTemperature, maxTemperature);
+				
+			
+			}
+			
+			if(!(maxHumidity>humidity)&&(minHumidity<humidity)){
+			double	outHumidity = humidity;
+			onOffControlMassage.messageHumidity(outHumidity, humidity);
+			System.out.println("");
+			 humidity = newControl.changeValeHumidity(humidity, minHumidity, maxHumidity);
+			 
+			 
+			 
+			 }
+			
+			if(!(maxLights>lights)&&(minLights<lights))
+				{
+				double	outLights = lights;
+				onOffControlMassage.messageLights(outLights, lights);
+				System.out.println("");
+			 lights = newControl.changeValeLights(lights, minLights, maxLights);
+			 
+			 
+			}
+			 
+		 }
+		
+		 System.out.println("");
+		 
+		 // out menu to display
+		 
+		System.out.println("1 - change temperature; 2 - change humidity; 3 - change lights; 4 - vale of greenhouse: 5 - exit ");	
+		System.out.println("take your chose:");
+		
+		
+		int exit = 1;
+		//do something while user not chose exit
+		while(exit!=0){
+		//	try{
+			
+		Scanner scan= new Scanner(System.in);
+        int vik;
+        if(scan.hasNextInt()){
+        	vik=scan.nextInt();
+			if (vik>5||vik <1)
+			{
+				System.out.println("your chose don't true");
+			}
+    
+        switch (vik){
+		//change vale of temperature 
+        			case 1:{System.out.println("1 - your chose change temperatur:");
+        			temperature = change.changeTemperature(minTemperature, maxTemperature, temperature);
+        			System.out.println("\nteke your next chose");
+        				break;
+        			}
+        //change vale of humidity
+        				case 2:{System.out.println("2 - your chose change humidity");
+        				humidity = change.changeHumidity(minHumidity, maxHumidity, humidity);
+        				System.out.println("\nteke your next chose");
+        				break;
+        			}
+        //change vale of lights
+        				case 3:{System.out.println("3 - your chose change lights");
+        				lights = change.changeLights(minLights, minLights, lights);
+        				System.out.println("\nteke your next chose");
+        				break;
+        			}
+		// show to display vale of temperature, humidity, lights;
+        				case 4:{System.out.println("4 - your chose vale of greenhouse:");
+        				sensorsPrintVale.valeTemperature(temperature);
+        				sensorsPrintVale.valeHumidity(humidity);
+        				sensorsPrintVale.valeLights(lights);
+        				System.out.println("\nteke your next chose");
+        				break;
+        				}
+        	// exit			
+        			    case 5:{System.out.println("5 - your chose exit");
+		
+        			    exit = 0;
+        			    break;
+        			 }
+
+        		}
+        }else{
+        	System.out.println("your enter not true vale");
+        }
+			}/*catch(Exception e)
+			{
+				JOptionPane.showMessageDialog(null, "your enter not true vale");
+			}
+		}*/
+		
+			
+		}
+		
 	
 	
-	public class massege{
-
-		public void messageTemperature(double outTemperature, double temperature) {
-			System.out.println("Failed!\nThe temperature " + outTemperature + " - unacceptable.\nIt was changed to the optimum temperature - " + temperature);
-		}
-
-		public void messageHumidity(double outHumidity, double humidity) {
-			System.out.println("Failed!\nThe Humidity " + outHumidity + " - unacceptable.\nIt was changed to the optimum humidity - " + humidity);
-		}
-
-		public void messageLights(double outLights, double lights) {
-			System.out.println("Failed!\nThe Lights " + outLights + " - unacceptable.\nIt was changed to the optimum lights - " + lights);
-		}
-	}
-
-	public double changeValeTemperature(double temperature, double minTemperature, double maxTemperature) {
-		if (temperature>maxTemperature||temperature<minTemperature){
-			System.out.println("Temperature is too high \n We start normalizing temperature...");
-			
-			double temperature1 = (minTemperature+maxTemperature)/2;
-			System.out.println("The temperature is normilized!");
-			return temperature1;
-			
-		} else		{
-			System.out.println("System failed to change temperature");
-			return temperature;
-		}
-		
-         		
-		
 	}
 
 
-	public double changeValeHumidity(double humidity, double minHumidity, double maxHumidity) {
-		if (humidity>maxHumidity||humidity<minHumidity){
-			System.out.println("Humidity is too high \n We start normalizing humidity...");
-			
-			double humidity1 = (minHumidity+maxHumidity)/2;
-			System.out.println("The humidity is normilized!");
-			return humidity1;
-			
-		} else		{
-			System.out.println("System failed to change humidity");
-			return humidity;
-		}
-		
-         		
-		
-	}
-
-
-	public double changeValeLights(double lights, double maxLights, double minLights) {
-		if (lights>maxLights||lights<minLights){
-			System.out.println("Lights is too high \n We start normalizing humidity...");
-			
-		   lights = (minLights+maxLights)/2;
-			System.out.println("The lights is normilized! ");
-			return lights;
-			
-		} else		{
-			System.out.println("System failed to change lights");
-			return lights;
-		}
-
-
-}
-}
