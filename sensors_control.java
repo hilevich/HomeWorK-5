03f0b145e0c131a7b740956ed372d1d196f41fdc
yura <yura2007.31@gmail.com>
2016-03-04 10:05:41 +0200

    Update and rename sensors.java to sensors_control.java

---
+++ sensors_control.java
@@ -0,0 +1,54 @@
+package greenhouse;
+
+public class control {
+
+	
+	public double changeTemperature(double minTemperature, double maxTemperature) {
+	
+		return 0;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public double changeHumidity(double minTemperature, double maxTemperature) {
+		
+		return 0;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public double changeLights(double minTemperature, double maxTemperature) {
+		return 0;
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public class sensorsPrint {
+		
+		public void valeTemperature(double temperature) {
+			System.out.println("Temperature is changed to  " + temperature + "  degree" );
+			
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void valeHumidity(double humidity) {
+			System.out.println("Humidity is changed to  " + humidity + "  %");
+			
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void valeLights(double lights) {
+			System.out.println("Lights is changed to  " + lights + "  1 mol/m2");
+			
+			// TODO Auto-generated method stub
+			
+		}
+		
+		
+	}
+	
+
+}
