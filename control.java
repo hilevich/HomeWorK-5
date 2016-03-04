4068618e7908da4c76ef595c3f523fc3a74caf4f
yura <yura2007.31@gmail.com>
2016-03-04 13:49:44 +0200

    Update control.java

--- control.java
+++ control.java
@@ -1,59 +1,143 @@
 package greenhouse;
 
+
 import java.util.Scanner;
+
+
 
 public class control {
 	
-public double changeTemperature(double minTemperature, double maxTemperature) {
+	//Scanner scan= new Scanner(System.in);
 	
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Please, set temperature between " + minTemperature + " and " + 
-	maxTemperature + "in decimal");
 	
-		while ((!sc.hasNextDouble()) || (sc.nextDouble() >  maxTemperature) || 
-				(sc.nextDouble() < minTemperature)) {
-			System.out.println("You entered invalid value, please, try again");
-			sc.nextLine();
-				}
+	public double changeTemperature(double minTemperature, double maxTemperature, double temperature) {
+		int exit = 1;
+		Scanner scan= new Scanner(System.in);
+		double vale = temperature;
+		while (exit!=0){
 		
-	Double temperature = sc.nextDouble();
-	return temperature;
-	
+		
+		System.out.println("enter your vale of temperature (max = "+maxTemperature+" min = "+minTemperature+" )");
+		
+			if (scan.hasNextDouble()){
+			vale = scan.nextDouble();
+			if ((maxTemperature>=vale)&&(vale>=minTemperature)){
+				System.out.println("your new temperature is - "+vale);
+				exit=0;
 			}
-
-
-
-	public double changeHumidity(double minHumidity, double maxHumidity) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Please, set humidity between " + minHumidity + " and " + 
-		maxHumidity + "in decimal");
+			else
+			{
+				System.out.println("your vale of temperature is not true plase change your vale");
+			}
+			
+			}else{
+				System.out.println("your enter not true vale");
+				
+				exit=0;
+			}
 		
-		while ((!sc.hasNextDouble()) || (sc.nextDouble() > maxHumidity) 
-				|| (sc.nextDouble() < minHumidity)) {
-			System.out.println("You entered invalid value, please, try again");
-			sc.nextLine();
-		}
-		Double humidity = sc.nextDouble();
-		return humidity;
 	}
-
 	
-	
-	public double changeLights(double maxLights, double minLights) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Please, set the lights ratio between " + minLights + "and" + 
-		maxLights + "in decimal");
-		
-		while ((!sc.hasNextDouble()) || (sc.nextDouble() > maxLights) 
-				|| (sc.nextDouble() < minLights)) {
-			System.out.println("You entered invalid value, please, try again");
-			sc.nextLine();
-		}
-		Double lights = sc.nextDouble();
-		return lights;
-		
+			
+		return vale;
+		// TODO Auto-generated method stub
 		
 	}
 
+	public double changeHumidity(double minHumidity, double maxHumidity, double humidity) {
+		int exit = 1;
+		Scanner scan= new Scanner(System.in);
+		double vale = humidity;
+		while (exit!=0){
+		
+		
+		System.out.println("enter your vale of temperature (max = "+maxHumidity+" min = "+minHumidity+" )");
+		
+			if (scan.hasNextDouble()){
+			vale = scan.nextDouble();
+			if ((maxHumidity>=vale)&&(vale>=minHumidity)){
+				System.out.println("your new temperature is - "+vale);
+				exit=0;
+			}
+			else
+			{
+				System.out.println("your vale of temperature is not true plase change your vale");
+			}
+			
+			}else{
+				System.out.println("your enter not true vale");
+				
+				exit=0;
+			}
+		
+	}
+	
+			
+		return vale;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public double changeLights(double minLights, double maxLights, double lights) {
+		int exit = 1;
+		Scanner scan= new Scanner(System.in);
+		double vale = lights;
+		while (exit!=0){
+		
+		
+		System.out.println("enter your vale of temperature (max = "+maxLights+" min = "+minLights+" )");
+		
+			if (scan.hasNextDouble()){
+			vale = scan.nextDouble();
+			if ((maxLights>=vale)&&(vale>=minLights)){
+				System.out.println("your new temperature is - "+vale);
+				exit=0;
+			}
+			else
+			{
+				System.out.println("your vale of temperature is not true plase change your vale");
+			}
+			
+			}else{
+				System.out.println("your enter not true vale");
+				
+				exit=0;
+			}
+		
+	}
+	
+			
+		return vale;
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public class sensorsPrint {
+		
+		public void valeTemperature(double temperature) {
+			System.out.println("Temperature is changed to  " + temperature + "  degree" );
+			
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void valeHumidity(double humidity) {
+			System.out.println("Humidity is changed to  " + humidity + "  %");
+			
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void valeLights(double lights) {
+			System.out.println("Lights is changed to  " + lights + "  1 mol/m2");
+			
+			// TODO Auto-generated method stub
+			
+		}
+		
+		
+	}
+	
 
 }
